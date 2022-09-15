af0544bba10b1c5f92bf8e3ebf1e12a24b987224
jaesnny <jchu8@cougarnet.uh.edu>
2022-09-15 01:07:19 -0500

    lab1.12 + lab1.20

---
+++ Homework1/Lab1.20.py
@@ -0,0 +1,11 @@
+user_num1 = int(input("Enter integer:\n"))
+print("You entered:", user_num1)
+user_num1_sq = user_num1 * user_num1
+user_num1_cb = user_num1 ** 3
+print(user_num1, "squared is", user_num1_sq)
+print("And", user_num1, "cubed is", user_num1_cb,"!!")
+user_num2 = int(input("Enter another integer:\n"))
+user_num2_sum = user_num2 + user_num1
+user_num2_prod = user_num1 * user_num2
+print(user_num1, "+", user_num2, "is", user_num2_sum)
+print(user_num1, "*", user_num2, "is", user_num2_prod)
