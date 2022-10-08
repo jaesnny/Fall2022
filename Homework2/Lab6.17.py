c4d30d5657414603c31011b442bdd5448c3d5036
jaesnny <jchu8@cougarnet.uh.edu>
2022-10-08 02:55:41 -0500

    complete 6.17

---
+++ Homework2/Lab6.17.py
@@ -0,0 +1,20 @@
+#JenniferChu
+#1873159
+
+mypassword = input()
+password = ''
+for i in mypassword:
+    if i == 'i':
+        password += '!'
+    elif i == 'a':
+        password += '@'
+    elif i == 'm':
+        password += 'M'
+    elif i == 'B':
+        password += '8'
+    elif i == 'o':
+        password += '.'
+    else:
+        password += i
+password += 'q*s'
+print(password)
