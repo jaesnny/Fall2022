b116d9bdd4e2abea93c6fb47cc649ab7f9cbaa92
jaesnny <jchu8@cougarnet.uh.edu>
2022-10-30 23:57:55 -0500

    Lab10.15 completed

---
+++ Homework3/Lab10.15.py
@@ -0,0 +1,24 @@
+class Team:
+    def __init__(self) -> object:
+        self.team_name = 'none'
+        self.team_wins = 0
+        self.team_losses = 0
+
+    def get_win_percentage(self):
+        return self.team_wins / (self.team_wins + self.team_losses)
+
+
+if __name__ == "__main__":
+    team_name = input()
+    team_wins = int(input())
+    team_losses: int = int(input())
+    team_score = Team()
+
+    team_score.team_name = team_name
+    team_score.team_wins = team_wins
+    team_score.team_losses = team_losses
+
+    if team_score.get_win_percentage() >= 0.5:
+        print("Congratulations, Team", team_score.team_name, "has a winning average!")
+    else:
+        print('Team', team_score.team_name, "has a losing average.")
